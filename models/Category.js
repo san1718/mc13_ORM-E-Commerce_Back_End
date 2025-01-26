2e8835604719dcda63e1bb1cd78892727d5f96ff
Sung Min An <an.sungmin17@icloud.com>
2025-01-25 16:03:22 -0800

    model

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,33 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+// Imports model
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
+const Product = require("./Product");
 
 class Category extends Model {}
 
+// Category table
 Category.init(
+  // define columns
   {
-    // define columns
+    // id
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    // category name
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,56 @@
-// import important parts of sequelize library
+// Imports important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+// Imports our database connection from config.js
 const sequelize = require('../config/connection');
+// Imports Category, Tag, and ProductTag models
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
+  // define columns
   {
-    // define columns
+    // id
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    // product name
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // price
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    // stock
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    // category id
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
